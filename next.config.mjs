67467f7357232ecc23995400d7bcaa91d2f435fb
subcero123 <subcero139@hotmail.com>
2025-03-27 11:42:15 -0600

    Actualizando .gitignore y eliminando archivos ignorados

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,11 @@
-let userConfig = undefined;
-try {
-  userConfig = await import("./v0-user-next.config");
-} catch (e) {
-  // ignore error
-}
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Genera un sitio estático
   images: {
     unoptimized: true, // Evita la optimización de imágenes para exportación
   },
   basePath: "/my-portfolio", // Reemplaza con el nombre de tu repo en GitHub
-  assetPrefix: "/my-portfolio/",
+  assetPrefix: "/my-portfolio",
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -22,33 +15,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
 };
-
-mergeConfig(nextConfig, userConfig);
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return;
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === "object" &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      };
-    } else {
-      nextConfig[key] = userConfig[key];
-    }
-  }
-}
 
 export default nextConfig;

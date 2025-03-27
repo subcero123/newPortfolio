ac5352e0d860368b3a70fc2ba2f2f18bd632a438
subcero123 <subcero139@hotmail.com>
2025-03-26 18:38:03 -0600

    Adds metadata and icons to layout component
    Incorporates metadata and icons into the layout component to enhance the portfolio website's SEO and visual appearance.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,31 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Hector Ugarte | Portfolio",
   description: "Fullstack developer, React, Angular, TypeScript, TailwindCSS",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  keywords: [
+    "Hector Ugarte",
+    "Hector Yoav Ugarte Ramirez",
+    "portfolio",
+    "fullstack developer",
+  ],
+  authors: [{ name: "Hector Yoav Ugarte Ramirez" }],
+  openGraph: {
+    title: "Hector Ugarte | Portfolio",
+    description: "Fullstack developer, React, Angular, TypeScript, TailwindCSS",
+    url: "https://subcero123.github.io/my-portfolio/",
+    siteName: "Hector Ugarte Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Hector Ugarte | Portfolio",
+    description: "Fullstack developer, React, Angular, TypeScript, TailwindCSS",
+  },
 };
 
 export default function RootLayout({
